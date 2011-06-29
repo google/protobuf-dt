1247067fbc846a5fed27149aa320b32b3c9e6baf
Alex Ruiz <alruiz@google.com>
2011-06-29 18:20:40 +0000

    Fixed: [ Issue 77 ] SNumber Syntax Coloring doesn't apply to float numbers like "1.0" https://code.google.com/p/protobuf-dt/issues/detail?id=77

--- com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/editor/syntaxcoloring/ProtobufSemanticHighlightingCalculator.java
+++ com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/editor/syntaxcoloring/ProtobufSemanticHighlightingCalculator.java
@@ -1,14 +1,15 @@
 /*
  * Copyright (c) 2011 Google Inc.
- *
- * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
- *
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
  */
 package com.google.eclipse.protobuf.ui.editor.syntaxcoloring;
 
-import static org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration.DEFAULT_ID;
+import static org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration.*;
 
 import com.google.eclipse.protobuf.protobuf.*;
 import com.google.eclipse.protobuf.protobuf.Package;
@@ -21,26 +22,28 @@ import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.*;
 
+import static com.google.eclipse.protobuf.protobuf.ProtobufPackage.Literals.*;
+
 /**
  * @author alruiz@google.com (Alex Ruiz)
  */
 public class ProtobufSemanticHighlightingCalculator implements ISemanticHighlightingCalculator {
 
   @Inject private ModelNodes nodes;
-  
+
   public void provideHighlightingFor(XtextResource resource, IHighlightedPositionAcceptor acceptor) {
     if (resource == null) return;
     EList<EObject> contents = resource.getContents();
     if (contents == null || contents.isEmpty()) return;
     Protobuf protobuf = (Protobuf) contents.get(0);
-    highlightAllNames(protobuf, acceptor, DEFAULT_ID);
+    highlightAllFeatures(protobuf, acceptor, DEFAULT_ID);
   }
 
-  private void highlightAllNames(Protobuf protobuf, IHighlightedPositionAcceptor acceptor, String highlightId) {
-    highlightElementNames(protobuf, acceptor, highlightId);
+  private void highlightAllFeatures(Protobuf protobuf, IHighlightedPositionAcceptor acceptor, String highlightId) {
+    highlightElementFeatures(protobuf, acceptor, highlightId);
   }
 
-  private void highlightElementNames(Protobuf protobuf, IHighlightedPositionAcceptor acceptor, String highlightId) {
+  private void highlightElementFeatures(Protobuf protobuf, IHighlightedPositionAcceptor acceptor, String highlightId) {
     for (ProtobufElement element : protobuf.getElements()) {
       if (element instanceof Package || element instanceof Option) {
         highlightName(element, acceptor, highlightId);
@@ -48,17 +51,17 @@ public class ProtobufSemanticHighlightingCalculator implements ISemanticHighligh
       }
       if (element instanceof Type) {
         highlightName(element, acceptor, highlightId);
-        if (element instanceof Message) highlightElementNames((Message) element, acceptor, highlightId);
+        if (element instanceof Message) highlightElementFeatures((Message) element, acceptor, highlightId);
         continue;
       }
       if (element instanceof Service) {
         highlightName(element, acceptor, highlightId);
-        highlightRpcNames((Service) element, acceptor, highlightId);
+        highlightRpcFeatures((Service) element, acceptor, highlightId);
       }
     }
   }
 
-  private void highlightElementNames(Message message, IHighlightedPositionAcceptor acceptor, String highlightId) {
+  private void highlightElementFeatures(Message message, IHighlightedPositionAcceptor acceptor, String highlightId) {
     for (MessageElement element : message.getElements()) {
       if (element instanceof Option) {
         highlightName(element, acceptor, highlightId);
@@ -66,31 +69,40 @@ public class ProtobufSemanticHighlightingCalculator implements ISemanticHighligh
       }
       if (element instanceof Field) {
         highlightName(element, acceptor, highlightId);
-        highlightFieldOptionNames((Field) element, acceptor, highlightId);
+        highlightFieldOptionFeatures((Field) element, acceptor, highlightId);
+      }
+      if (element instanceof Property) {
+        highlightElementFeatures((Property) element, acceptor);
       }
       if (element instanceof Group) {
-        highlightElementNames((Group) element, acceptor, highlightId);
+        highlightElementFeatures((Group) element, acceptor, highlightId);
       }
     }
   }
 
-  private void highlightElementNames(Group group, IHighlightedPositionAcceptor acceptor, String highlightId) {
+  private void highlightElementFeatures(Property property, IHighlightedPositionAcceptor acceptor) {
+    ValueRef defaultValue = property.getDefault();
+    if (!(defaultValue instanceof NumberRef)) return;
+    highlightFirstFeature(property, PROPERTY__DEFAULT, acceptor, NUMBER_ID);
+  }
+
+  private void highlightElementFeatures(Group group, IHighlightedPositionAcceptor acceptor, String highlightId) {
     for (GroupElement e : group.getElements()) {
       if (e instanceof Property) {
         Field field = (Field) e;
         highlightName(field, acceptor, highlightId);
-        highlightFieldOptionNames(field, acceptor, highlightId);
+        highlightFieldOptionFeatures(field, acceptor, highlightId);
       }
     }
   }
 
-  private void highlightFieldOptionNames(Field field, IHighlightedPositionAcceptor acceptor, String highlightId) {
+  private void highlightFieldOptionFeatures(Field field, IHighlightedPositionAcceptor acceptor, String highlightId) {
     for (FieldOption option : field.getFieldOptions()) {
       highlightName(option, acceptor, highlightId);
     }
   }
 
-  private void highlightRpcNames(Service service, IHighlightedPositionAcceptor acceptor, String highlightId) {
+  private void highlightRpcFeatures(Service service, IHighlightedPositionAcceptor acceptor, String highlightId) {
     for (Rpc rpc : service.getRpcs()) {
       highlightName(rpc, acceptor, highlightId);
       for (Option option : rpc.getOptions()) {
@@ -100,13 +112,13 @@ public class ProtobufSemanticHighlightingCalculator implements ISemanticHighligh
   }
 
   private void highlightName(EObject o, IHighlightedPositionAcceptor acceptor, String highlightId) {
-    highlightFirstFeature(o, o.eClass().getEStructuralFeature("name"), highlightId, acceptor);
+    highlightFirstFeature(o, o.eClass().getEStructuralFeature("name"), acceptor, highlightId);
   }
 
-  private void highlightFirstFeature(EObject semantic, EStructuralFeature feature, String highlightId,
-      IHighlightedPositionAcceptor acceptor) {
+  private void highlightFirstFeature(EObject semantic, EStructuralFeature feature,
+      IHighlightedPositionAcceptor acceptor, String highlightId) {
     INode node = nodes.firstNodeForFeature(semantic, feature);
     if (node == null || node.getText() == null) return;
-    acceptor.addPosition(node.getOffset(), node.getText().length(), highlightId);
+    acceptor.addPosition(node.getOffset(), node.getText().trim().length(), highlightId);
   }
 }
