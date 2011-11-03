25f574c6f37ddd9e3ce146a3e4305e03d28c2def
Alex Ruiz <alruiz@google.com>
2011-11-02 23:55:44 -0700

    Fixed: [Issue 152] Escaped literals in Strings not supported.

---
+++ com.google.eclipse.protobuf.test/src/com/google/eclipse/protobuf/bugs/Issue152_SupportEscapedCharactersInStrings_Test.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2011 Google Inc.
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.google.eclipse.protobuf.bugs;
+
+import static com.google.eclipse.protobuf.junit.core.Setups.unitTestSetup;
+import static com.google.eclipse.protobuf.junit.core.XtextRule.createWith;
+import static org.junit.Assert.assertNotNull;
+
+import com.google.eclipse.protobuf.junit.core.XtextRule;
+
+import org.junit.*;
+
+/**
+ * Tests fix for <a href="http://code.google.com/p/protobuf-dt/issues/detail?id=152">Issue 152</a>.
+ * 
+ * @author alruiz@google.com (Alex Ruiz)
+ */
+public class Issue152_SupportEscapedCharactersInStrings_Test {
+
+  @Rule public XtextRule xtext = createWith(unitTestSetup());
+  
+  //  message Foo {
+  //    optional bytes escaped_bytes = 1 [default = "\0\001\a\b\f\n\r\t\v\\\'\"\xfe"];
+  //  }
+  @Test public void should_recognize_escaped_characters() {
+    assertNotNull(xtext.root());
+  }  
+}
--- com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/builder/CompoundParser.java
+++ com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/builder/CompoundParser.java
@@ -1,5 +1,11 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
-
+/*
+ * Copyright (c) 2011 Google Inc.
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package com.google.eclipse.protobuf.ui.builder;
 
 import static java.util.Arrays.asList;
