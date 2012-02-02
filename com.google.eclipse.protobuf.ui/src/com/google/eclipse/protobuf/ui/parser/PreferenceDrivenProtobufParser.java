d6efba03bb3cea8fabc67fe60030d73f106b406c
Alex Ruiz <alruiz@google.com>
2012-02-02 11:38:50 -0800

    Code cleanup.

--- com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/parser/PreferenceDrivenProtobufParser.java
+++ com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/parser/PreferenceDrivenProtobufParser.java
@@ -15,7 +15,6 @@ import com.google.eclipse.protobuf.ui.preferences.parser.core.ParserChecksPrefer
 import com.google.inject.Inject;
 
 import org.antlr.runtime.CharStream;
-import org.eclipse.xtext.nodemodel.*;
 import org.eclipse.xtext.nodemodel.impl.NodeModelBuilder;
 import org.eclipse.xtext.parser.*;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreAccess;
@@ -24,13 +23,8 @@ import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreAccess;
  * @author alruiz@google.com (Alex Ruiz)
  */
 public class PreferenceDrivenProtobufParser extends ProtobufParser {
-  private static final String[] ERRORS_TO_LOOK_FOR = { "missing EOF at 'c'", "missing EOF at 'java'",
-      "missing EOF at 'parsed'", "missing EOF at 'python'", "missing EOF at 'sawzall'",
-      "no viable alternative at input '<'" };
-
   @Inject private IPreferenceStoreAccess storeAccess;
 
-  // @Inject private ParserChecksSettingsProvider settingsProvider;
   @Override protected IParseResult doParse(String ruleName, CharStream in, NodeModelBuilder builder,
       int initialLookAhead) {
     IParseResult result = super.doParse(ruleName, in, builder, initialLookAhead);
@@ -42,28 +36,7 @@ public class PreferenceDrivenProtobufParser extends ProtobufParser {
   }
 
   private boolean isNotProto2(IParseResult result) {
-    for (INode node : result.getSyntaxErrors()) {
-      if (isNonProto2(node.getSyntaxErrorMessage())) {
-        return true;
-      }
-    }
     Protobuf root = (Protobuf) result.getRootASTElement();
-    if (root != null && root.getSyntax() == null) {
-      return true;
-    }
-    return false;
-  }
-
-  private boolean isNonProto2(SyntaxErrorMessage syntaxErrorMessage) {
-    if (syntaxErrorMessage == null) {
-      return false;
-    }
-    String message = syntaxErrorMessage.getMessage();
-    for (String nonProto2Keyword : ERRORS_TO_LOOK_FOR) {
-      if (message.contains(nonProto2Keyword)) {
-        return true;
-      }
-    }
-    return false;
+    return root == null || root.getSyntax() == null;
   }
 }
