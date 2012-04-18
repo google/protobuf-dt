690d3c891818c2c876d8cd10c0f0042ade274e49
Alex Ruiz <alruiz@google.com>
2012-04-18 12:52:34 -0700

    In progress: [Issue 215] Add a "new .proto file" wizard
    Made the wizard look like Eclipse's "New File" wizard.

--- com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/wizards/NewProtoFileWizardPage.java
+++ com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/wizards/NewProtoFileWizardPage.java
@@ -8,155 +8,32 @@
  */
 package com.google.eclipse.protobuf.ui.wizards;
 
-import static com.google.eclipse.protobuf.ui.util.Workspaces.workspaceRoot;
-import static org.eclipse.jface.window.Window.OK;
-import static org.eclipse.swt.SWT.*;
-import static org.eclipse.swt.layout.GridData.FILL_HORIZONTAL;
+import static com.google.eclipse.protobuf.ui.wizards.Messages.*;
 
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.events.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
-import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+import java.io.*;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+
+import com.google.eclipse.protobuf.ui.internal.ProtobufActivator;
 
 /**
  * @author alruiz@google.com (Alex Ruiz)
  */
-public class NewProtoFileWizardPage extends WizardPage {
-  private Text containerText;
-  private Text fileText;
-  private final ISelection selection;
-
-  public NewProtoFileWizardPage(ISelection selection) {
-    super("wizardPage");
-    setTitle("Multi-page Editor File");
-    setDescription("This wizard creates a new file with *.proto extension that can be opened by a multi-page editor.");
-    this.selection = selection;
+public class NewProtoFileWizardPage extends WizardNewFileCreationPage {
+  public NewProtoFileWizardPage(IStructuredSelection selection) {
+    super("NewProtoFileWizardPage", selection); //$NON-NLS-1$
+    setTitle(pageTitle);
+    setDescription(pageDescription);
+    setFileExtension("proto");
+    setFileName("newFile.proto");
   }
 
-  @Override public void createControl(Composite parent) {
-    Composite container = new Composite(parent, NULL);
-    GridLayout layout = new GridLayout();
-    container.setLayout(layout);
-    layout.numColumns = 3;
-    layout.verticalSpacing = 9;
-    Label label = new Label(container, NULL);
-    label.setText("&Container:");
-
-    containerText = new Text(container, BORDER | SINGLE);
-    containerText.setLayoutData(new GridData(FILL_HORIZONTAL));
-    containerText.addModifyListener(new ModifyListener() {
-      @Override public void modifyText(ModifyEvent e) {
-        dialogChanged();
-      }
-    });
-
-    Button button = new Button(container, PUSH);
-    button.setText("Browse...");
-    button.addSelectionListener(new SelectionAdapter() {
-      @Override public void widgetSelected(SelectionEvent e) {
-        handleBrowse();
-      }
-    });
-    label = new Label(container, NULL);
-    label.setText("&File name:");
-
-    fileText = new Text(container, BORDER | SINGLE);
-    fileText.setLayoutData(new GridData(FILL_HORIZONTAL));
-    fileText.addModifyListener(new ModifyListener() {
-      @Override public void modifyText(ModifyEvent e) {
-        dialogChanged();
-      }
-    });
-    initialize();
-    dialogChanged();
-    setControl(container);
-  }
-
-  private void initialize() {
-    Object selected = firstSelectedElement();
-    if (selected != null) {
-      if (selected instanceof IResource) {
-        IContainer container;
-        if (selected instanceof IContainer) {
-          container = (IContainer) selected;
-        } else {
-          container = ((IResource) selected).getParent();
-        }
-        containerText.setText(container.getFullPath().toString());
-      }
-    }
-    fileText.setText("new_file.proto");
-  }
-
-  private Object firstSelectedElement() {
-    if (selection == null || selection.isEmpty()) {
+  @Override protected InputStream getInitialContents() {
+    try {
+      return ProtobufActivator.getInstance().getBundle().getEntry("/resources/newFileContents.proto").openStream();
+    } catch (IOException e) {
       return null;
     }
-    if (selection instanceof IStructuredSelection) {
-      ((IStructuredSelection) selection).getFirstElement();
-    }
-    return null;
-  }
-
-  private void handleBrowse() {
-    ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(), workspaceRoot(), false,
-        "Select new file container");
-    if (dialog.open() == OK) {
-      Object[] result = dialog.getResult();
-      if (result.length == 1) {
-        containerText.setText(((Path) result[0]).toString());
-      }
-    }
-  }
-
-  private void dialogChanged() {
-    IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(containerName()));
-    String fileName = fileName();
-    if (containerName().isEmpty()) {
-      updateStatus("File container must be specified");
-      return;
-    }
-    if (container == null || (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
-      updateStatus("File container must exist");
-      return;
-    }
-    if (!container.isAccessible()) {
-      updateStatus("Project must be writable");
-      return;
-    }
-    if (fileName.isEmpty()) {
-      updateStatus("File name must be specified");
-      return;
-    }
-    if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
-      updateStatus("File name must be valid");
-      return;
-    }
-    int dotLoc = fileName.lastIndexOf('.');
-    if (dotLoc != -1) {
-      String ext = fileName.substring(dotLoc + 1);
-      if (ext.equalsIgnoreCase("proto") == false) {
-        updateStatus("File extension must be \"proto\"");
-        return;
-      }
-    }
-    updateStatus(null);
-  }
-
-  private void updateStatus(String message) {
-    setErrorMessage(message);
-    setPageComplete(message == null);
-  }
-
-  public String containerName() {
-    return containerText.getText();
-  }
-
-  public String fileName() {
-    return fileText.getText();
   }
 }
