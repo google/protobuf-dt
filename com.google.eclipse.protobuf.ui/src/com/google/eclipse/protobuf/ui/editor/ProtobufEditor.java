eff972bfbb7e07d1c54a35f1c68667fa2d9f1622
Alex Ruiz <alruiz@google.com>
2012-05-09 14:36:39 -0700

    Created own proto editor.

---
+++ com.google.eclipse.protobuf.ui/src/com/google/eclipse/protobuf/ui/editor/ProtobufEditor.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.google.eclipse.protobuf.ui.editor;
+
+import org.eclipse.xtext.ui.editor.XtextEditor;
+
+/**
+ * @author alruiz@google.com (Alex Ruiz)
+ */
+public class ProtobufEditor extends XtextEditor {
+}
